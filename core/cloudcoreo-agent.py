074937394400a7dda04c8a77fa9cfde0be2b6e4e
David Mattes <mattes@nixnux.org>
2016-07-20 15:14:21 -0700

    DEV-436: add --version back into cloudcoreo-agent.py arguments.

--- core/cloudcoreo-agent.py
+++ core/cloudcoreo-agent.py
@@ -507,5 +507,10 @@ def start_agent():
 
     recursive_daemon()
 
+parser=argparse.ArgumentParser(description='Parse version argument')
+parser.add_argument('--version', action='store_true', help="Get script version")
+if parser.parse_args().version:
+    print "%s" % version
+    terminate_script()
 
 start_agent()
