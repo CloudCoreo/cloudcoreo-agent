bd6d6abef295146b0c44edd66466103f618833be
David Mattes <mattes@nixnux.org>
2016-11-05 13:53:13 -0700

    PLA-1590: some error checking on opscript existence and uniqueness

--- core/cloudcoreo_agent.py
+++ core/cloudcoreo_agent.py
@@ -665,8 +665,12 @@ def run_script(message_body):
         server_name = get_server_name()
         op_scripts = collect_operational_scripts(repo_dir, server_name)
         full_script_path = [test_file for test_file in op_scripts if script_name in test_file]
-        # assuming we only have a single script named script_name in full_script_path, so use [0]
-        if len(full_script_path) and len(full_script_path[0]):
+        if len(full_script_path) == 0:
+            log("operational script [%s] not found for server [%s]" % (script_name, server_name))
+        elif len(full_script_path) > 1:
+            log("Error: found [%d] operational scripts [%s] for server [%s]" %
+                (len(full_script_path), script_name, server_name))
+        elif len(full_script_path) and len(full_script_path[0]):
             env = get_environment_dict()
             run_cmd(full_script_path[0], env)
     except Exception as ex:
